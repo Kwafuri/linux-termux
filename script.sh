3d2e7d40a9a9fbf3f8bb0eaae0db92724f43132a
Kwafuri <91066691+Kwafuri@users.noreply.github.com>
2021-09-20 21:44:45 +0800

    Removed 12.04 support as it won't work as intended

--- script.sh
+++ script.sh
@@ -1,32 +1,17 @@
+
 #!/bin/bash
 
 ubuntu () {
 echo "Choose which Ubuntu version you want to install."
-echo "1. Ubuntu 12.04"
-echo "2. Ubuntu 14.04"
-echo "3. Ubuntu 16.04"
-echo "4. Ubuntu 18.04"
-echo "5. Ubuntu 20.04"
-echo "6. Ubuntu 20.10"
-echo "7. Ubuntu 21.04"
-echo "8. Cancelling Ubuntu installation"
+echo "1. Ubuntu 14.04"
+echo "2. Ubuntu 16.04"
+echo "3. Ubuntu 18.04"
+echo "4. Ubuntu 20.04"
+echo "5. Ubuntu 20.10"
+echo "6. Ubuntu 21.04"
+echo "7. Cancelling Ubuntu installation"
 read -p "Please enter a number: " choice2
 if [ $choice2 == "1" ]
-    then
-    if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
-    echo "It seems like you've installed Ubuntu."
-    echo "Please run ./startubuntu.sh to start it or re-run this script and choose 2 to uninstall."
-    exit 0
-fi
-    if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
-    echo "Starting Ubuntu 12.04 installation..."
-    echo "Installing proot and wget first, just in case it's not installed..."
-    apt-get install proot wget -y
-    echo "Installing Ubuntu 12.04..."
-    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-12.04/ubuntu.sh)
-fi
-fi
-if [ $choice2 == "2" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -41,7 +26,7 @@ fi
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-14.04/ubuntu.sh)
 fi
 fi
-if [ $choice2 == "3" ]
+if [ $choice2 == "2" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -56,7 +41,7 @@ fi
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-16.04/ubuntu.sh)
 fi
 fi
-if [ $choice2 == "4" ]
+if [ $choice2 == "3" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -71,7 +56,7 @@ fi
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-18.04/ubuntu.sh)
 fi
 fi
-if [ $choice2 == "5" ]
+if [ $choice2 == "4" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -86,7 +71,7 @@ fi
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-20.04/ubuntu.sh)
 fi
 fi
-if [ $choice2 == "6" ]
+if [ $choice2 == "5" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -102,7 +87,7 @@ fi
 fi
 fi
 
-if [ $choice2 == "7" ]
+if [ $choice2 == "6" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -117,7 +102,7 @@ fi
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-21.04/ubuntu.sh)
 fi
 fi
-if [ $choice2 == "8" ]
+if [ $choice2 == "7" ]
     then
     echo "Cancelling Ubuntu installation..."
     exit 0
