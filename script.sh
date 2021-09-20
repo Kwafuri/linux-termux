f6c171f024ba7478b7890f24656e3919d4820ba1
Kwafuri <91066691+Kwafuri@users.noreply.github.com>
2021-09-20 21:30:23 +0800

    Yay, 12.04 and 14.04 are here!

--- script.sh
+++ script.sh
@@ -2,12 +2,14 @@
 
 ubuntu () {
 echo "Choose which Ubuntu version you want to install."
-echo "1. Install Ubuntu 16.04"
-echo "2. Install Ubuntu 18.04"
-echo "3. Install Ubuntu 20.04"
-echo "4. Install Ubuntu 20.10"
-echo "5. Install Ubuntu 21.04"
-echo "6. Cancelling Ubuntu installation"
+echo "1. Ubuntu 12.04"
+echo "2. Ubuntu 14.04"
+echo "3. Ubuntu 16.04"
+echo "4. Ubuntu 18.04"
+echo "5. Ubuntu 20.04"
+echo "6. Ubuntu 20.10"
+echo "7. Ubuntu 21.04"
+echo "8. Cancelling Ubuntu installation"
 read -p "Please enter a number: " choice2
 if [ $choice2 == "1" ]
     then
@@ -17,11 +19,11 @@ if [ $choice2 == "1" ]
     exit 0
 fi
     if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
-    echo "Starting Ubuntu 16.04 installation..."
+    echo "Starting Ubuntu 12.04 installation..."
     echo "Installing proot and wget first, just in case it's not installed..."
-    apt-get install proot wget
-    echo "Installing Ubuntu 16.04..."
-    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-16.04/ubuntu.sh)
+    apt-get install proot wget -y
+    echo "Installing Ubuntu 12.04..."
+    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-12.04/ubuntu.sh)
 fi
 fi
 if [ $choice2 == "2" ]
@@ -32,11 +34,11 @@ if [ $choice2 == "2" ]
     exit 0
 fi
     if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
-    echo "Starting Ubuntu 18.04 installation..."
+    echo "Starting Ubuntu 14.04 installation..."
     echo "Installing proot and wget first, just in case it's not installed..."
-    apt-get install proot wget
-    echo "Installing Ubuntu 18.04..."
-    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-18.04/ubuntu.sh)
+    apt-get install proot wget -y
+    echo "Installing Ubuntu 14.04..."
+    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-14.04/ubuntu.sh)
 fi
 fi
 if [ $choice2 == "3" ]
@@ -47,11 +49,11 @@ if [ $choice2 == "3" ]
     exit 0
 fi
     if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
-    echo "Starting Ubuntu 20.04 installation..."
-    echo "Installing proof and wget first, just in case it's not installed..."
-    apt-get install proot wget
-    echo "Installing Ubuntu 20.04..."
-    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-20.04/ubuntu.sh)
+    echo "Starting Ubuntu 16.04 installation..."
+    echo "Installing proot and wget first, just in case it's not installed..."
+    apt-get install proot wget -y
+    echo "Installing Ubuntu 16.04..."
+    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-16.04/ubuntu.sh)
 fi
 fi
 if [ $choice2 == "4" ]
@@ -62,15 +64,45 @@ if [ $choice2 == "4" ]
     exit 0
 fi
     if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
+    echo "Starting Ubuntu 18.04 installation..."
+    echo "Installing proot and wget first, just in case it's not installed..."
+    apt-get install proot wget -y
+    echo "Installing Ubuntu 18.04..."
+    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-18.04/ubuntu.sh)
+fi
+fi
+if [ $choice2 == "5" ]
+    then
+    if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
+    echo "It seems like you've installed Ubuntu."
+    echo "Please run ./startubuntu.sh to start it or re-run this script and choose 2 to uninstall."
+    exit 0
+fi
+    if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
+    echo "Starting Ubuntu 20.04 installation..."
+    echo "Installing proof and wget first, just in case it's not installed..."
+    apt-get install proot wget -y
+    echo "Installing Ubuntu 20.04..."
+    bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-20.04/ubuntu.sh)
+fi
+fi
+if [ $choice2 == "6" ]
+    then
+    if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
+    echo "It seems like you've installed Ubuntu."
+    echo "Please run ./startubuntu.sh to start it or re-run this script and choose 2 to uninstall."
+    exit 0
+fi
+    if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
     echo "Starting Ubuntu 20.10 installation..."
     echo "Installing proot and wget first, just in case it's not installed..."
-    apt-get install proot wget
+    apt-get install proot wget -y
     echo "Installing Ubuntu 20.10..."
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-20.10/ubuntu.sh)
 fi
 fi
 
-if [ $choice2 == "5" ]
+if [ $choice2 == "7" ]
     then
     if [ -d "ubuntu-binds, ubuntu-fs" ] || [ -f "startubuntu.sh" ];then
     echo "It seems like you've installed Ubuntu."
@@ -80,12 +112,12 @@ fi
     if [ ! -d "ubuntu-binds, ubuntu-fs" ] || [ ! -f "startubuntu.sh" ];then
     echo "Starting Ubuntu 21.04 installation..."
     echo "Installing proot and wget first, just in case it's not installed..."
-    apt-get install proot wget
+    apt-get install proot wget -y
     echo "Installing Ubuntu 21.04..."
     bash <(curl -sSL https://raw.githubusercontent.com/Kwafuri/linux-termux/main/resources/ubuntu/ubuntu-21.04/ubuntu.sh)
 fi
 fi
-if [ $choice2 == "6" ]
+if [ $choice2 == "8" ]
     then
     echo "Cancelling Ubuntu installation..."
     exit 0
